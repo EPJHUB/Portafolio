643adc7c9ef438d966e0755e46e35bf528977c5a
Erick Patiño <pj_erick@hotmail.com>
2024-03-02 15:53:16 -0600

    fixing as per pro development requirements on going

--- languageHandler.js
+++ languageHandler.js
@@ -2,7 +2,7 @@ import language from "./language.json" assert {type: 'json'}
 //navbar
 const nav1 = document.getElementById('lan-nav1');
 const nav2 = document.getElementById('lan-nav2');
-const nav3 = document.getElementById('lan-nav3');
+// const nav3 = document.getElementById('lan-nav3');
 const nav4 = document.getElementById('lan-nav4');
 const nav5 = document.getElementById('lan-nav5');
 const nav6 = document.getElementById('lan-nav6');
@@ -19,25 +19,25 @@ const aboutCardText1 = document.getElementById('lan-about-card-text1');
 const aboutCardTitle2 = document.getElementById('lan-about-card-title2');
 const aboutCardText2 = document.getElementById('lan-about-card-text2');
 //milestones-secction
-const msSecctionTitle = document.getElementById('lan-ms-section-title');
-const msTimeline1Title = document.getElementById('lan-ms-timeline1-title');
-const msDegree1 = document.getElementById('lan-ms-degree1')
-const msDegree1Init = document.getElementById('lan-ms-degree1-init')
-const msDegree1End = document.getElementById('lan-ms-degree1-end')
-const msDegree2 = document.getElementById('lan-ms-degree2')
-const msDegree2Init = document.getElementById('lan-ms-degree2-init')
-const msDegree2End = document.getElementById('lan-ms-degree2-end')
-const msDegree3 = document.getElementById('lan-ms-degree3')
-const msDegree3Init = document.getElementById('lan-ms-degree3-init')
-const msDegree3End = document.getElementById('lan-ms-degree3-end')
+// const msSecctionTitle = document.getElementById('lan-ms-section-title');
+// const msTimeline1Title = document.getElementById('lan-ms-timeline1-title');
+// const msDegree1 = document.getElementById('lan-ms-degree1')
+// const msDegree1Init = document.getElementById('lan-ms-degree1-init')
+// const msDegree1End = document.getElementById('lan-ms-degree1-end')
+// const msDegree2 = document.getElementById('lan-ms-degree2')
+// const msDegree2Init = document.getElementById('lan-ms-degree2-init')
+// const msDegree2End = document.getElementById('lan-ms-degree2-end')
+// const msDegree3 = document.getElementById('lan-ms-degree3')
+// const msDegree3Init = document.getElementById('lan-ms-degree3-init')
+// const msDegree3End = document.getElementById('lan-ms-degree3-end')
 
-const msTimeline2Title = document.getElementById('lan-ms-timeline2-title');
-const msJob1 = document.getElementById('lan-ms-job1')
-const msJob1Init = document.getElementById('lan-ms-job1-init')
-const msJob1End = document.getElementById('lan-ms-job1-end')
-const msJob2 = document.getElementById('lan-ms-job2')
-const msJob2Init = document.getElementById('lan-ms-job2-init')
-const msJob2End = document.getElementById('lan-ms-job2-end')
+// const msTimeline2Title = document.getElementById('lan-ms-timeline2-title');
+// const msJob1 = document.getElementById('lan-ms-job1')
+// const msJob1Init = document.getElementById('lan-ms-job1-init')
+// const msJob1End = document.getElementById('lan-ms-job1-end')
+// const msJob2 = document.getElementById('lan-ms-job2')
+// const msJob2Init = document.getElementById('lan-ms-job2-init')
+// const msJob2End = document.getElementById('lan-ms-job2-end')
 //project
 const projectSecctionTitle = document.getElementById('lan-project-section-title');
 //skills
@@ -65,7 +65,7 @@ const setLanguage = (lan) => {
     //navbar
     nav1.innerText = language[lan].nav[1];
     nav2.innerText = language[lan].nav[2];
-    nav3.innerText = language[lan].nav[3];
+    // nav3.innerText = language[lan].nav[3];
     nav4.innerText = language[lan].nav[4];
     nav5.innerText = language[lan].nav[5];
     nav6.innerText = language[lan].nav[6];
@@ -84,24 +84,24 @@ const setLanguage = (lan) => {
     aboutCardTitle2.innerText = language[lan].about.cardTitle2;
     aboutCardText2.innerText = language[lan].about.cardText2;
     //milestones
-    msSecctionTitle.innerText = language[lan].milestones.secctionTitle;
-    msTimeline1Title.innerText = language[lan].milestones.timelineTitle1;
-    msDegree1.innerText = language[lan].milestones.degree1;
-    msDegree1Init.innerHTML = language[lan].milestones.degree1init;
-    msDegree1End.innerHTML = language[lan].milestones.degree1end;
-    msDegree2.innerText = language[lan].milestones.degree2;
-    msDegree2Init.innerHTML = language[lan].milestones.degree2init;
-    msDegree2End.innerHTML = language[lan].milestones.degree2end;
-    msDegree3.innerText = language[lan].milestones.degree3;
-    msDegree3Init.innerHTML = language[lan].milestones.degree3init;
-    msDegree3End.innerHTML = language[lan].milestones.degree3end;
-    msTimeline2Title.innerText = language[lan].milestones.timelineTitle2;
-    msJob1.innerText = language[lan].milestones.job1;
-    msJob1Init.innerHTML = language[lan].milestones.job1init;
-    msJob1End.innerHTML = language[lan].milestones.job1end;
-    msJob2.innerText = language[lan].milestones.job2;
-    msJob2Init.innerHTML = language[lan].milestones.job2init;
-    msJob2End.innerHTML = language[lan].milestones.job2end;
+    // msSecctionTitle.innerText = language[lan].milestones.secctionTitle;
+    // msTimeline1Title.innerText = language[lan].milestones.timelineTitle1;
+    // msDegree1.innerText = language[lan].milestones.degree1;
+    // msDegree1Init.innerHTML = language[lan].milestones.degree1init;
+    // msDegree1End.innerHTML = language[lan].milestones.degree1end;
+    // msDegree2.innerText = language[lan].milestones.degree2;
+    // msDegree2Init.innerHTML = language[lan].milestones.degree2init;
+    // msDegree2End.innerHTML = language[lan].milestones.degree2end;
+    // msDegree3.innerText = language[lan].milestones.degree3;
+    // msDegree3Init.innerHTML = language[lan].milestones.degree3init;
+    // msDegree3End.innerHTML = language[lan].milestones.degree3end;
+    // msTimeline2Title.innerText = language[lan].milestones.timelineTitle2;
+    // msJob1.innerText = language[lan].milestones.job1;
+    // msJob1Init.innerHTML = language[lan].milestones.job1init;
+    // msJob1End.innerHTML = language[lan].milestones.job1end;
+    // msJob2.innerText = language[lan].milestones.job2;
+    // msJob2Init.innerHTML = language[lan].milestones.job2init;
+    // msJob2End.innerHTML = language[lan].milestones.job2end;
     //projects
     projectSecctionTitle.innerText = language[lan].projects.secctionTitle;
     //skills
