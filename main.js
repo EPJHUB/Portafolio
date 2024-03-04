65991dbe49801a279200a955b840859a9e51f664
Erick Patiño <pj_erick@hotmail.com>
2024-03-03 23:27:13 -0600

    swiper added on project section

--- main.js
+++ main.js
@@ -192,3 +192,18 @@ SR.reveal(".contact-form", {
   distance: "300px",
   // reset: true,
 });
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
